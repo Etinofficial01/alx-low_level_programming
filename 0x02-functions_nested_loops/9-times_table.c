882ffea36a2610164cd5356b573c15f81f4521ce
Etinofficial01 <emmanuelosemwengie@gmail.com>
2022-09-13 20:29:49 -0700

    time table for 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,22 +17,22 @@ void times_table(void)
 
 			if (j == 0)
 			{
-				_putchar(k + '0');
+				_putchar(n + '0');
 
 			}
-			if (k < 10 && j != 0)
+			if (n < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0');
+				_putchar(n + '0');
 			}
-			else if (k >= 10)
+			else if (n >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
 			}
 		}
 		_putchar('\n');
