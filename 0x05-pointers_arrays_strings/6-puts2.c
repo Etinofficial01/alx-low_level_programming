8cfcff455e171fdbce5097233686dbd2040fb52b
Etinofficial01 <emmanuelosemwengie@gmail.com>
2022-09-19 16:27:01 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints out every other character followed by a new line
+ * @str: string to be printed
+ */
+
+void puts2(char *str)
+{
+	int i, len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
