e8f060eaf29b092fea38630df003337119d73ed1
Etinofficial01 <emmanuelosemwengie@gmail.com>
2022-10-03 05:47:37 -0700

    printing arguments in new lines

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that prints all arguments its receives
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
