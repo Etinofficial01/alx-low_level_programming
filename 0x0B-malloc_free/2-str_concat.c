a651b27a8e1435a69cbcb21637e043fdae7234c2
Etinofficial01 <emmanuelosemwengie@gmail.com>
2022-10-05 16:26:26 -0700

    new task 02

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *str_concat;
+	char *concat_str;
 	int index, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
@@ -20,15 +20,15 @@ char *str_concat(char *s1, char *s2)
 
 	for (index = 0; s1[index] || s2[index]; index++)
 		len++;
-	str_concat = malloc(sizeof(char) * len);
+	concat_str = malloc(sizeof(char) * len);
 
-	if (str_concat == NULL)
+	if (concat_str == NULL)
 		return (NULL);
 
 	for (index = 0; s1[index]; index++)
-		str_concat[concat_index++] * s1[index];
+		concat_str[concat_index++] = s1[index];
 
 	for (index = 0; s2[index]; index++)
-		str_concat[concat_index++] * s2[index];
-	return (str_concat);
+		concat_str[concat_index++] = s2[index];
+	return (concat_str);
 }
